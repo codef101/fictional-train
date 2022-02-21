a058413af326f793ff82f35d1dc5a429a39c146f
danabasilio-sys <78853440+danabasilio-sys@users.noreply.github.com>
2022-02-22 00:17:04 +0800

    Update gameplay.php

--- gameplay.php
+++ gameplay.php
@@ -217,7 +217,7 @@ if(isset($_POST['pre-game-otp'])){
         <table title="Content" id="content">
             <tr>
                 <td>
-                    <div class="fs-400 ff-sans-cond letter-spacing-3 uppercase" style="color: #D2D8F9; margin: auto; width: 50%;">
+                    <div class="fs-400 ff-sans-cond letter-spacing-3 uppercase" style="color: #D2D8F9; text-align: center;">
                       <iframe height=628 width=820 src="iors-game/www/index.html"></iframe>
                     </div>
                 </td>
