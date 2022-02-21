b5d2476554ce2cd5816280162efb9e4e6f8a2586
danabasilio-sys <78853440+danabasilio-sys@users.noreply.github.com>
2022-02-21 18:16:07 +0800

    Update gameplay.php

--- gameplay.php
+++ gameplay.php
@@ -78,6 +78,7 @@ if(isset($_POST['pre-game-otp'])){
     #game-commands td {
       background-color: hsl( var(--clr-light) );
       color: black;
+      font-weight: 200;
     }
 
     #game-commands th {
