c0d0b9b01d17d34ad19d2df2187b79ac8b79c054
danabasilio-sys <78853440+danabasilio-sys@users.noreply.github.com>
2022-02-23 22:42:19 +0800

    Update gameplay.php

--- gameplay.php
+++ gameplay.php
@@ -271,20 +271,20 @@ if(isset($_POST['pre-game-otp'])){
     <!-- GAME -->
     
     <div class="game-container" style="background-color: black">
-        <script type="text/javascript" src="iors-game/www/js/libs/pixi.js"></script>
-        <script type="text/javascript" src="iors-game/www/js/libs/pixi-tilemap.js"></script>
-        <script type="text/javascript" src="iors-game/www/js/libs/pixi-picture.js"></script>
-        <script type="text/javascript" src="iors-game/www/js/libs/fpsmeter.js"></script>
-        <script type="text/javascript" src="iors-game/www/js/libs/lz-string.js"></script>
-        <script type="text/javascript" src="iors-game/www/js/libs/iphone-inline-video.browser.js"></script>
-        <script type="text/javascript" src="iors-game/www/js/rpg_core.js"></script>
-        <script type="text/javascript" src="iors-game/www/js/rpg_managers.js"></script>
-        <script type="text/javascript" src="iors-game/www/js/rpg_objects.js"></script>
-        <script type="text/javascript" src="iors-game/www/js/rpg_scenes.js"></script>
-        <script type="text/javascript" src="iors-game/www/js/rpg_sprites.js"></script>
-        <script type="text/javascript" src="iors-game/www/js/rpg_windows.js"></script>
-        <script type="text/javascript" src="iors-game/www/js/plugins.js"></script>
-        <script type="text/javascript" src="iors-game/www/js/main.js"></script>
+        <script type="text/javascript" src="iors-game-website/iors-game/www/js/libs/pixi.js"></script>
+        <script type="text/javascript" src="iors-game-website/iors-game/www/js/libs/pixi-tilemap.js"></script>
+        <script type="text/javascript" src="iors-game-website/iors-game/www/js/libs/pixi-picture.js"></script>
+        <script type="text/javascript" src="iors-game-website/iors-game/www/js/libs/fpsmeter.js"></script>
+        <script type="text/javascript" src="iors-game-website/iors-game/www/js/libs/lz-string.js"></script>
+        <script type="text/javascript" src="iors-game-website/iors-game/www/js/libs/iphone-inline-video.browser.js"></script>
+        <script type="text/javascript" src="iors-game-website/iors-game/www/js/rpg_core.js"></script>
+        <script type="text/javascript" src="iors-game-website/iors-game/www/js/rpg_managers.js"></script>
+        <script type="text/javascript" src="iors-game-website/iors-game/www/js/rpg_objects.js"></script>
+        <script type="text/javascript" src="iors-game-website/iors-game/www/js/rpg_scenes.js"></script>
+        <script type="text/javascript" src="iors-game-website/iors-game/www/js/rpg_sprites.js"></script>
+        <script type="text/javascript" src="iors-game-website/iors-game/www/js/rpg_windows.js"></script>
+        <script type="text/javascript" src="iors-game-website/iors-game/www/js/plugins.js"></script>
+        <script type="text/javascript" src="iors-game-website/iors-game/www/js/main.js"></script>
     </div>
 
     <!-- END OF GAME -->
