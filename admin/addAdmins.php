5c8b723cd79a79c3a1505298d1f7fff844cc597d
daniellelzr <99892312+daniellelzr@users.noreply.github.com>
2022-02-28 12:15:04 +0800

    Update addAdmins.php

--- admin/addAdmins.php
+++ admin/addAdmins.php
@@ -169,16 +169,16 @@ if (isset($_POST['register'])) {
                         <span class="text-danger"><?php echo $cpassword_err ?></span>
                     </div>
                 </div>
-                    <div class="mb-3">
+                   <!-- <div class="mb-3">
                         <div class="form-group">
                             <label>Role</label><br>
                             <select class="select" name="role">
                                 <option value="0">Super Admin</option>
                                 <option value="1">Admin</option>
                             </select>
-                            <span class="text-danger"><?php echo $role_err;?></span>
+                            <span class="text-danger"><?//php echo $role_err;?></span>
                         </div>
-                    </div>
+                    </div> -->
                 <div class="row">
                     <div class="col-12">
                         <button type="submit" name="register" class="btn btn-primary">Submit</button>
