e73a9c809a0a558623d6a01e5ef4ac4ef4a716a3
daniellelzr <99892312+daniellelzr@users.noreply.github.com>
2022-03-05 16:26:46 +0800

    Update gameplay.php

--- gameplay.php
+++ gameplay.php
@@ -273,7 +273,7 @@ $_SESSION['check_url'] = "gameplay";
     <!--<button  type="button" onclick="location.href='post-game-quiz'" id="start-button" class="start-button uppercase ff-serif text-dark bg-white">Start</button>-->
     
     
-    <form method="POST" action="post-game-quiz" style="display: none;">
+    <form method="POST" action="post-game-quiz"> <!--style="display: none;-->
       <input type="hidden" name="postGamePlay" value="post-game-play"/>
       <button  type="submit" class="start-button uppercase ff-serif text-dark bg-white" id="start-button">Start</button>
     </form>
