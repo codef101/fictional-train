fa6569dd8c856f2eab4c4e219c3bd3959ade3dbf
danabasilio-sys <78853440+danabasilio-sys@users.noreply.github.com>
2022-03-06 20:48:27 +0800

    Update menu.php

--- parts/menu.php
+++ parts/menu.php
@@ -10,7 +10,7 @@ $page = end($request);
 
 
 
-$game_in = in_array($page, array('pre-game-quiz','end-pre-game','pre-game-verification','gameplay','post-game-quiz','end-post-game'));
+$game_in = in_array($page, array('pre-game-quiz.php','end-pre-game.php','pre-game-verification.php','gameplay.php','post-game-quiz.php','end-post-game.php'));
 
 
 
@@ -22,11 +22,10 @@ $game_in = in_array($page, array('pre-game-quiz','end-pre-game','pre-game-verifi
 
 	<li class="<?php echo empty($page) ? 'active' : ''; ?>"><a class="ff-sans-cond uppercase text-white letter-spacing-2" href="/"><span aria-hidden="true">00</span>Home</a>
 
-    <li class="<?php echo $page=='characters' ? 'active' : ''; ?>"><a class="ff-sans-cond uppercase text-white letter-spacing-2" href="characters"><span aria-hidden="true">01</span>Characters</a>
+    <li class="<?php echo $page=='characters.php' ? 'active' : ''; ?>"><a class="ff-sans-cond uppercase text-white letter-spacing-2" href="characters"><span aria-hidden="true">01</span>Characters</a>
 
-    <li class="<?php echo ($page=='game' || $game_in) ? 'active' : ''; ?>"><a class="ff-sans-cond uppercase text-white letter-spacing-2" href="<?php if(isset($_SESSION['check_url']) && $_SESSION['check_url'] == "gameplay"){echo "gameplay";}else{echo "game"; }?>"><span aria-hidden="true">02</span>Game</a>
+    <li class="<?php echo ($page=='game.php' || $game_in) ? 'active' : ''; ?>"><a class="ff-sans-cond uppercase text-white letter-spacing-2" href="<?php if(isset($_SESSION['check_url']) && $_SESSION['check_url'] == "gameplay.php"){echo "gameplay.php";}else{echo "game.php"; }?>"><span aria-hidden="true">02</span>Game</a>
 
-    <li class="<?php echo $page=='about' ? 'active' : ''; ?>"><a class="ff-sans-cond uppercase text-white letter-spacing-2" href="about"><span aria-hidden="true">03</span>About</a>
+    <li class="<?php echo $page=='about.php' ? 'active' : ''; ?>"><a class="ff-sans-cond uppercase text-white letter-spacing-2" href="about"><span aria-hidden="true">03</span>About</a>
 
 </ul>
-
