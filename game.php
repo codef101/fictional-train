5f25abcd460829b43a78b6dc419131dc5494c6ba
danabasilio-sys <78853440+danabasilio-sys@users.noreply.github.com>
2022-03-07 11:35:19 +0800

    Update game.php

--- game.php
+++ game.php
@@ -10,7 +10,13 @@ foreach($_SESSION as $key=> $value){
     unset($_SESSION[$key]);
   }
 }
-
+$cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+  foreach($cookies as $cookie) {
+        $parts = explode('=', $cookie);
+        $name = trim($parts[0]);
+        setcookie($name, '', time()-1000);
+        setcookie($name, '', time()-1000, '/');
+  } 
 
 ?>
 
