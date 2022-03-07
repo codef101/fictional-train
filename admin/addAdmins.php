0f9dec76bb5ac4e483d6ee5f85b5729affc95256
daniellelzr <99892312+daniellelzr@users.noreply.github.com>
2022-03-07 14:08:50 +0500

    Update addAdmins.php

--- admin/addAdmins.php
+++ admin/addAdmins.php
@@ -23,95 +23,113 @@ $role_err= null;
 $yes = null;
 $error = null;
 if (isset($_POST['register'])) {
-    $fname = trim($_POST['fname']);
-    $lname = trim($_POST['lname']);
-    $email = trim($_POST['email']);
-    $password = trim($_POST['password']);
-    $cpassword = trim($_POST['cpassword']);
-    $role= trim($_POST['role']);
-    //error checking
-    $valid = true;
-    if (empty($fname)) {
-        $valid = false;
-        $fname_err = "First Name is required";
+    $validinput = true;
+    $check = notOnlySpecialChars($_POST['fname']);
+    if($check['status'] === false){
+        $validinput = false;
+        $fname_err = $check['message'];
     }
-    if (ctype_alpha(str_replace(' ', '', $fname)) === false)  {
-        $fname_err = 'First Name must contain letters and spaces only';
-        $valid=false;
+    $check = notOnlySpecialChars($_POST['lname']);
+    if($check['status'] === false){
+        $validinput = false;
+        $lname_err = $check['message'];
     }
-    if (empty($lname)) {
-        $valid = false;
-        $lname_err = "Last Name is required";
-    }
-    if (ctype_alpha(str_replace(' ', '', $lname)) === false)  {
-        $lname_err = 'Last Name must contain letters and spaces only';
-        $valid=false;
-    }
-    if (empty($email)) {
-        $valid = false;
-        $email_err = "Email is required";
-    }
-    if (empty($password)) {
-        $valid = false;
-        $password_err = "Password is required";
-    }
-    if (empty($cpassword)) {
-        $valid = false;
-        $cpassword_err = "Confirm Password is required";
-    }
-    if (strlen($password) < 8)
-    {
-        $valid=false;
-        $password_err="Password must be at least 8 characters";
-    }
-    if (strlen($password) > 16)
-    {
-        $valid=false;
-        $password_err="Password must not exceed 16 characters";
-    }
-    //last 2 ifs if together
-    /*if (strlen($password) < 8 || strlen($password) > 16)
-    {
-        $valid = false;
-        $password_err = "Password should be between 8-16 characters";
-    }*/
-    if(preg_match('@[^\w]@', $password)){
-        $valid=false;
-        $password_err="Only letters and numbers allowed";
-    }
-    if ($password != $cpassword) {
-        $valid = false;
-        $cpassword_err = "Password does not match";
-    }
-    if($role == 0)
-    {
-        $sql = "SELECT * FROM `admin` WHERE `admin_role` = 0";
-        $res=$con->query($sql);
-        if($res->num_rows == 2)
-        {
-            $valid =false;
-            $role_err="Only 2 super admins allowed";
-        }
-    }
-    if ($valid) {
-                $sql = "SELECT * FROM `admin` WHERE `email` = '$email'";
-                $res=$con->query($sql);
-        if ($res->num_rows == 0) {
-
-            $date=date('Y-m-d H:i:s');
-            $pass=hash('sha256',$password);
-             $sql = "INSERT INTO `admin` (`first_name`, `last_name`, `email`, `password`,`admin_role`,`created_at`) VALUES ('$fname','$lname','$email','$pass',$role,'$date')";
-             $res=$con->query($sql);
-            if ($res === true) {
-                $action='Added Admin: '.$email;
-                logEntry($action,$_SESSION['uid'],$con);
-                $yes = "Registered successfully";
-            } else {
-                $error = "Admin registration error. Try again.";
-            }
-        } else {
+    $check = notOnlySpecialChars($_POST['email']);
+    if($check['status'] === false){
+        $validinput = false;
+        $email_err = $check['message'];
+    }    
+    if($validinput){
+        $fname = trim($_POST['fname']);
+        $lname = trim($_POST['lname']);
+        $email = trim($_POST['email']);
+        $password = trim($_POST['password']);
+        $cpassword = trim($_POST['cpassword']);
+        $role= trim($_POST['role']);
+        //error checking
+        $valid = true;
+        if (empty($fname)) {
             $valid = false;
-            $email_err = "Email is already taken";
+            $fname_err = "First Name is required";
+        }
+        if (ctype_alpha(str_replace(' ', '', $fname)) === false)  {
+            $fname_err = 'First Name must contain letters and spaces only';
+            $valid=false;
+        }
+        if (empty($lname)) {
+            $valid = false;
+            $lname_err = "Last Name is required";
+        }
+        if (ctype_alpha(str_replace(' ', '', $lname)) === false)  {
+            $lname_err = 'Last Name must contain letters and spaces only';
+            $valid=false;
+        }
+        if (empty($email)) {
+            $valid = false;
+            $email_err = "Email is required";
+        }
+        if (empty($password)) {
+            $valid = false;
+            $password_err = "Password is required";
+        }
+        if (empty($cpassword)) {
+            $valid = false;
+            $cpassword_err = "Confirm Password is required";
+        }
+        if (strlen($password) < 8)
+        {
+            $valid=false;
+            $password_err="Password must be at least 8 characters";
+        }
+        if (strlen($password) > 16)
+        {
+            $valid=false;
+            $password_err="Password must not exceed 16 characters";
+        }
+        //last 2 ifs if together
+        /*if (strlen($password) < 8 || strlen($password) > 16)
+        {
+            $valid = false;
+            $password_err = "Password should be between 8-16 characters";
+        }*/
+        if(preg_match('@[^\w]@', $password)){
+            $valid=false;
+            $password_err="Only letters and numbers allowed";
+        }
+        if ($password != $cpassword) {
+            $valid = false;
+            $cpassword_err = "Password does not match";
+        }
+        //if($role == 0)
+        //{
+        //    $sql = "SELECT * FROM `admin` WHERE `admin_role` = 0";
+        //    $res=$con->query($sql);
+        //    if($res->num_rows == 2)
+        //    {
+        //        $valid =false;
+        //        $role_err="Only 2 super admins allowed";
+        //    }
+        //}
+        if ($valid) {
+                    $sql = "SELECT * FROM `admin` WHERE `email` = '$email'";
+                    $res=$con->query($sql);
+            if ($res->num_rows == 0) {
+
+                $date=date('Y-m-d H:i:s');
+                $pass=hash('sha256',$password);
+                 $sql = "INSERT INTO `admin` (`first_name`, `last_name`, `email`, `password`,`admin_role`,`created_at`) VALUES ('$fname','$lname','$email','$pass',$role,'$date')";
+                 $res=$con->query($sql);
+                if ($res === true) {
+                    $action='Added Admin: '.$email;
+                    logEntry($action,$_SESSION['uid'],$con);
+                    $yes = "Registered successfully";
+                } else {
+                    $error = "Admin registration error. Try again.";
+                }
+            } else {
+                $valid = false;
+                $email_err = "Email is already taken";
+            }
         }
     }
 }
