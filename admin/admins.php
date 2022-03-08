97809f762fdd804be5aa6099efec5f0bf66571d2
daniellelzr <99892312+daniellelzr@users.noreply.github.com>
2022-03-08 13:31:41 +0500

    Update admins.php

--- admin/admins.php
+++ admin/admins.php
@@ -1,20 +1,16 @@
 <?php
+error_reporting(E_ALL ^ E_WARNING); 
 include('includes/header.php');
 include('includes/functions.php');
-   if(isset($_GET['did'])){
-       $did=$_GET['did'];
+   if(isset($_REQUEST['did']) && intval($_REQUEST['did']) > 0){
+       $did=$_REQUEST['did'];
        $date=date('Y-m-d');
        $sql_get="SELECT * FROM `admin` WHERE `id`=$did";
        $get_res=$con->query($sql_get);
        $get_admin=$get_res->fetch_assoc();
        $can=canDeleteAdmins($_SESSION['role']);
        if (!$can){
-           ?>
-           <script>
-               window.location.href='index'
-           </script>
-           <?php
-           exit();
+           header("location:admins");
        }
       
        if($can){
@@ -26,12 +22,11 @@ include('includes/functions.php');
                 $res=$con->query($sql);
                 $condition = $res->num_rows == 1;
             }
-            if($condition && $_SESSION['role'] == 0 ){?>
-            <script>
-               window.location.href='admins';
-            </script>
-            <?php    
+            if($condition && $_SESSION['role'] == 0 ){
+                $_REQUEST['last_delete'] = "One Super Admin Required";
+                header("location:admins");
             }else{
+                $_REQUEST['last_delete'] = false;
                 $admin_email=$get_admin['email'];
                 $sql="DELETE FROM `admin` WHERE `id`=$did";
                 $res=$con->query($sql);
@@ -41,18 +36,9 @@ include('includes/functions.php');
                    if(intval($_SESSION['uid'])==intval($did)){
                        $action='Delete Supper Admin: '.$admin_email.' itself and Logged out.';
                        logEntry($action,$_SESSION['uid'],$con);
-                       session_destroy();
-                       ?>
-                       <script>
-                           window.location.href='login';
-                       </script>
-                       <?php
+                       session_destroy();header("location:admins");
                    }
-                   ?>
-                   <script>
-                       window.location.href='admins';
-                   </script>
-                   <?php
+                   header("location:admins");
                 }
             } 
         }
@@ -70,6 +56,14 @@ include('includes/functions.php');
                     </div>
                 </div>
                 <div class="card-body">
+                    <?php if(isset($_REQUEST['last_delete']) && $_REQUEST['last_delete']){ ?>
+                        <p class="alert alert-danger text-center">
+                            <?php 
+                                echo $_REQUEST['last_delete']; 
+                                unset($_REQUEST)
+                            ?>
+                        </p>
+                    <?php } ?>
                     <div class="table-responsive">
                     <table id="tabletest" class="table table-dark table-striped table-hover">
                         <thead>
@@ -114,8 +108,11 @@ include('includes/functions.php');
                             <td><?php echo $role; ?></td>
                             <?php if(isset($_SESSION['is_s_admin']) && $_SESSION['is_s_admin']){ ?>
                                <td class="d-flex">
-                                   <a href="editAdmins?id=<?php echo $r['id']; ?>" class="btn btn-sm  btn-primary"><i class="far fa-edit"></i></a>
-                                   <a href="admins?did=<?php echo $r['id']; ?>" class="btn btn-sm ml-1  btn-danger delete-confirm"><i class="fas fa-trash-alt"></i></a>
+                                    <a href="editAdmins?id=<?php echo $r['id']; ?>" class="btn btn-sm  btn-primary"><i class="far fa-edit"></i></a>
+                                    <form method="post" id="delete_user">
+                                        <input type="hidden" name="did" value="<?php echo $r['id']; ?>"/>    
+                                        <a href="admins" class="btn btn-sm ml-1  btn-danger delete-confirm"><i class="fas fa-trash-alt"></i></a>
+                                    </form>
                                </td>
                             <?php } ?>
                         </tr>
@@ -142,7 +139,7 @@ include('includes/functions.php');
                 confirmButtonText: 'Confirm delete'
             }).then((result) => {
                if (result.isConfirmed) {
-                   window.location.href=url
+                   $('#delete_user').submit()
                }
             })
         });
