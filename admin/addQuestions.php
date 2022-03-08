09bd75a0c93c91be67ff7ee692bb3e852055e1d7
daniellelzr <99892312+daniellelzr@users.noreply.github.com>
2022-03-08 20:52:40 +0800

    Update addQuestions.php

--- admin/addQuestions.php
+++ admin/addQuestions.php
@@ -17,7 +17,7 @@ $yes=null;
 $error=null;
 if(isset($_POST['addquestion']))
 {
-    $validinput = true;
+   /* $validinput = true;
     $check = notOnlySpecialChars($_POST['question']);
     if($check['status'] === false){
         $validinput = false;
@@ -42,7 +42,7 @@ if(isset($_POST['addquestion']))
     if($check['status'] === false){
         $validinput = false;
         $answer4_err = $check['message'];
-    }
+    }*/
     if($validinput){
         $question=toggleSlash($_POST['question'], 'add');
         $answer1=toggleSlash($_POST['answer1'], 'add');
